ea1161151041733c39b60838f3244e53da176abf
Limulos <randomimus.beakuzz@gmail.com>
2023-10-04 22:03:15 +0200

    The program can now read a random word from the extracted ones

--- src/main.nim
+++ src/main.nim
@@ -1,27 +1,11 @@
-import std/[random, sequtils, strutils, unicode]
-
-proc removeAccents(s: string): string =
-  return s
-    .toLower
-    .multiReplace(
-      ("é", "e"), ("è", "e"), ("ê", "e"), ("ï", "i"), ("ï", "i"), 
-      ("ç", "c"), ("â", "a"), ("à", "a"), ("ô", "o"), ("û", "u")
-    )
+import std/[random, strutils]
 
 proc main() =
   randomize()
 
-  let words = readFile("assets/liste_francais.txt")
-    .splitLines
-    .filterIt(it.runeLen == 5)
-    .mapIt(it.removeAccents)
-    .deduplicate(isSorted = true)
-    
-  let file = open("assets/words.txt", fmWrite)
-  defer: file.close()
-
-  for word in words:
-    file.writeLine(word)
+  let words = readFile("assets/words.txt").splitLines
+  let randomWord = words.sample
+  echo randomWord
 
 when isMainModule:
   main()
