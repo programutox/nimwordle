52b0fb3a3e2e40492ce2d9a9f7db7354e364a78e
Limulos <randomimus.beakuzz@gmail.com>
2023-10-06 15:05:13 +0200

    Created window

--- src/main.nim
+++ src/main.nim
@@ -1,11 +1,26 @@
 import std/[random, strutils]
+import raylib
+
+const
+  screenWidth = 600
+  screenHeight = 400
 
 proc main() =
   randomize()
 
   let words = readFile("assets/words.txt").splitLines
-  let randomWord = words.sample
-  echo randomWord
+  let randomWord = words.sample.cstring
 
+  initWindow(screenWidth, screenHeight, "Nim Wordle")
+  defer: closeWindow()
+
+  while not windowShouldClose():
+    beginDrawing()
+    defer: endDrawing()
+
+    clearBackground(RayWhite)
+    drawText(randomWord, 250, 10, 30, Black)
+
+# I moved game logic to a main function because defer is not supported at top-level
 when isMainModule:
   main()
