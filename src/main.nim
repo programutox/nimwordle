0ffd11f224888121d79191c6f9adb9df31dac617
Limulos <randomimus.beakuzz@gmail.com>
2023-10-09 20:44:30 +0200

    Added keyboard checks

--- src/main.nim
+++ src/main.nim
@@ -2,24 +2,45 @@ import std/[random, strutils]
 import raylib
 
 const
-  screenWidth = 600
-  screenHeight = 400
+  boxSize = 50
+  wordLimit = 5
+  screenWidth = boxSize * (wordLimit - 1)
+  attempts = 10
+  screenHeight = boxSize * (attempts + 1)
 
 proc main() =
   randomize()
 
   let words = readFile("assets/words.txt").splitLines
-  let randomWord = words.sample.cstring
+  let randomWord = words.sample.toUpper.cstring
+
+  var userInput = newStringOfCap(wordLimit)
 
   initWindow(screenWidth, screenHeight, "Nim Wordle")
   defer: closeWindow()
+
+  echo randomWord
 
   while not windowShouldClose():
     beginDrawing()
     defer: endDrawing()
 
+    # TODO: Replace getKeyPressed() by isKeyPressed(the_key), to avoid issue with keyboard layout
+    let key = getKeyPressed()
+    case key:
+      of KeyboardKey.A..KeyboardKey.Z:
+        if userInput.len != wordLimit:
+          userInput.add(key.ord.char)
+      of KeyboardKey.Backspace:
+        if userInput.len != 0:
+          userInput = userInput[0..^2]
+      of KeyboardKey.Enter:
+        discard
+      else:
+        discard
+
     clearBackground(RayWhite)
-    drawText(randomWord, 250, 10, 30, Black)
+    drawText(userInput.cstring, 10, 10, boxSize, Black)
 
 # I moved game logic to a main function because defer is not supported at top-level
 when isMainModule:
