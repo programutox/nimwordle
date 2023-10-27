c35dd572a4ebb44690d278e2491e665b1c188860
Limulos <randomimus.beakuzz@gmail.com>
2023-10-27 13:54:22 +0200

    Fixed a typo

--- src/state.nim
+++ src/state.nim
@@ -1,6 +1,6 @@
 type State* = ref object of RootObj
 
-# method keyword with {.base.} pragme enables to create a virtual method (like C++)
+# method keyword with {.base.} pragma enables to create a virtual method (like C++)
 # In that case it is needed for seq[State], 
 # so that the correct update and draw methods are called
 method update*(self: var State, states: var seq[State]) {.base.} =
