16335709d3693b8bffa41849eaa017340b0b7810
Limulos <randomimus.beakuzz@gmail.com>
2023-10-27 14:47:39 +0200

    Updated tutorial language

--- src/tutorialstate.nim
+++ src/tutorialstate.nim
@@ -14,27 +14,45 @@ const
     "A grey letter is not in",
     "the word.",
     "",
-    "Press [Enter] to start"
+    "Click to start"
+  ]
+  frenchInstructions = [
+    "Une lettre verte est à".cstring,
+    "la bonne place.",
+    "",
+    "Une lettre jaune est dans",
+    "le mot mais à la mauvaise",
+    "place.",
+    "",
+    "Une lettre grise n'est pas",
+    "dans le mot.",
+    "",
+    "Cliquez pour commencer"
   ]
   fontSize: int32 = 20
 
 type Tutorial = ref object of State
   textures: seq[Texture2D]
+  instructions: array[0..10, cstring]
 
 proc initTutorial*(language: Language): Tutorial =
-  result = Tutorial(textures: @[], language: language)
+  result = Tutorial(
+    textures: @[],
+    instructions: if language == English: englishInstructions else: frenchInstructions,
+    language: language
+  )
   result.textures.add("resources/green_letter.png".loadTexture)
   result.textures.add("resources/yellow_letter.png".loadTexture)
 
 method update*(self: var Tutorial, states: var seq[State]) =
-  if isKeyPressed(Enter) or isKeyPressed(KpEnter):
+  if isMouseButtonPressed(MouseButton.Left):
     states.add(initGame())
 
 method draw*(self: var Tutorial) =
   var j: int32 = 0
   let texture_height: int32 = self.textures[0].height
 
-  for i, instruction in englishInstructions:
+  for i, instruction in self.instructions:
     if instruction != "":
       drawText(instruction, 10, 10 + j * (texture_height + 5) + (i.int32 - j) * (fontSize + 5), fontSize, Black)
     elif j < self.textures.len:
